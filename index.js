7bb196328eddf366faf03f892e94dd598a68864b
Firealem Erko <firealemg@yahoo.com>
2019-05-28 15:23:44 -0400

    added express, server listening

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+
+const express = require("express");
+const db = require("./data/db");
+const cors = require('cors');
+
+const server = express();
+server.listen(4000, () => {
+    console.log("\n*** Running on port 4000 ***\n");
+  });
